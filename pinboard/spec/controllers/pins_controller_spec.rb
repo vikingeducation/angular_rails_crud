480cf311c1d7496c03dc6be477a8450c880ebb39
alokp <alokpra@gmail.com>
2015-09-21 11:29:10 -0700

    working on tests for new pin

--- pinboard/spec/controllers/pins_controller_spec.rb
+++ pinboard/spec/controllers/pins_controller_spec.rb
@@ -3,23 +3,44 @@ require 'rails_helper'
 RSpec.describe PinsController, type: :controller do
 
   let(:user){ create(:user)}
-  let(:pin){ create(:pin)}
+  let!(:pin){ create(:pin, :user => user)}
   let(:json){ JSON.parse(response.body) }
 
-  context 'render pins index' do
+  describe 'GET /pins' do
 
-    describe 'GET /pins' do
-
-      before do
-        get :index, format: :json
-      end
-
-      it 'should return a collection of pins' do
-
-      end
-
+    before do
+      get :index, format: :json
     end
 
-  end  # end context
+    it 'should return a collection of pins' do
+      expect(json.map{|pin| pin['id']}).to include(pin.id)
+    end
+
+    it 'should return a collection of pins that includes a user' do
+      expect(json.map{|pin| pin['user_id'] }).to include(user.id)
+    end
+
+  end
+
+  describe 'POST /pins' do
+
+    before do
+      let!(:pin2){ create(:pin, :user => user)}
+    end
+
+    it 'should create a new pin with valid submission' do
+      expect do
+        post :create, pin: attributes_for(:pin)
+      end.to change(Pin, :count).by(1)
+    end
+
+    it 'should not create a new pin with invalid information' do
+      expect do
+        post :create, user_id: nil,
+              pin: attributes_for(:pin)
+      end.to change(Pin, :count).by(1)
+    end
+
+  end
 
 end

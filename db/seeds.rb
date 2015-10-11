69639dc6e0a5375a798fb4ecb5726bb20146157c
Adam <adam.kinson@gmail.com>
2015-10-11 08:55:09 -0400

    build models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+USERS = ['Abe', 'Bob', 'Cam', 'Dan']
+
+ITEMS = [
+          {
+            name: 'Table',
+            description: 'A place to eat.'
+          },
+          {
+            name: 'Desk',
+            description: 'A place to work.'
+          },
+          {
+            name: 'Dynamite',
+            description: 'Ideal for cartoon coyotes.'
+          },
+          {
+            name: 'Cream cheese',
+            description: 'Safe for internet purchasing.'
+          }
+        ]
+
+USERS.each do |user|
+  u = User.new(username: user)
+
+  rand(1..4).times do
+    p = u.pins.build
+    i = ITEMS[rand(ITEMS.length)]
+    p.item_name = i[:name]
+    p.description = i[:description]
+    p.buy_sell = false
+  end
+
+  u.save!
+end

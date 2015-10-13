a42e67d26ae8c24b3a13f48acff232f36d848c27
Adam <adam.kinson@gmail.com>
2015-10-12 21:47:26 -0400

    build create functionality... must test

--- app/assets/javascripts/controllers/pins/PinsIndexCtrl.js
+++ app/assets/javascripts/controllers/pins/PinsIndexCtrl.js
@@ -3,4 +3,19 @@ crudpin.controller('PinsIndexCtrl',
   function($scope, Restangular, pins) {
 
     $scope.pins = pins;
+
+    $scope.processForm = function(valid) {
+      if (valid) {
+        Restangular.all('pins').post($scope.newPin)
+          .then( $scope.addPinToIndex );
+      };
+    };
+
+    $scope.addPinToIndex = function(response) {
+      $scope.newPin.user = response.user;
+      $scope.newPin.created_at = response.created_at;
+      $scope.pins.push($scope.newPin);
+      $scope.newPin = {};
+    };
+
 }])
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,7 +6,28 @@ class PinsController < ApplicationController
     respond_to do |format|
       format.json { render json: @pins.to_json(:include => :user) }
     end
-
   end
+
+
+  def create
+    @pin = current_user.pins.build(pin_params)
+
+    if @pin.save
+      respond_to do |format|
+        format.json { render json: @pin.to_json(:include => :user), :status => 201 }
+      end
+    else
+      respond_to do |format|
+        format.json { render :nothing => :true, :status => 400 }
+      end
+    end
+  end
+
+
+  private
+
+    def pin_params
+      params.require(:pin).permit(:item_name, :buy_sell, :description)
+    end
 
 end

4c07aaa3d69e3c8dcb137c1e478ea7b6263630b1
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-31 14:05:28 -0400

    making pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,12 +4,32 @@ class PinsController < ApplicationController
     @pins = Pin.all
 
     respond_to do |format|
-      format.json {render json: @pins.to_json }
+      format.json {render json: @pins }
     end
   end
 
+  def create
+    @pin = Pin.new( pin_params )
+
+    if @pin.save
+      respond_to do |format|
+        format.json { render json: @pin }
+      end
+    else
+      respond_to do |format|
+        format.json { render status: :unprocessable_entity }
+      end
+    end
+
+  end
 
 
+  private
+
+
+  def pin_params
+    params.require(:pin).permit(:item_name, :description, :user_id, :buy_sell)
+  end
 
 
 end

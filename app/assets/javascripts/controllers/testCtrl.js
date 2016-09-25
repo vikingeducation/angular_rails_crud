f9186a1e92c0ab74f4130c549bb554e41efe6680
JeremyVe <jepschx@gmail.com>
2016-09-25 14:14:00 +0800

    initial setup with seed

---
+++ app/assets/javascripts/controllers/testCtrl.js
@@ -0,0 +1,4 @@
+pinBoard.controller('testCtrl', 
+	['$scope', function($scope) {
+		$scope.test = 'hi there';
+	}])
---
+++ db/migrate/20160925053314_create_pins.rb
@@ -0,0 +1,13 @@
+class CreatePins < ActiveRecord::Migration
+  def change
+    create_table :pins do |t|
+      t.string :item_name, null: false
+      t.boolean :buy_sell, null: false
+      t.string :description, null: false
+      t.integer :user_id, null: false
+
+
+      t.timestamps null: false
+    end
+  end
+end

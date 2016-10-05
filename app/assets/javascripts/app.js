f8c0bc174e87ef05124fa15b850d97e6503b61f2
Leo Ahnn <leo.ahnn@gmail.com>
2016-10-05 12:27:26 -0400

    init the rails app

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,47 @@
+var app = angular.module('app', ['ui.router', 'restangular']);
+
+app.factory('_', [
+  '$window',
+  function($window) {
+    return $window._;
+  }
+]);
+
+app.config([
+  "$httpProvider",
+  function($httpProvider) {
+    var token = $('meta[name=csrf-token]').attr('content');
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = token;
+  }
+]);
+
+// config for restangular
+app.config([
+  'RestangularProvider',
+  function(RestangularProvider) {
+
+    RestangularProvider.setBaseUrl('/api/v1');
+    RestangularProvider.setRequestSuffix('.json');
+    RestangularProvider.setDefaultHttpFields({"content-type": "application/json"});
+  }
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('main', {
+      url: '/',
+      views: {
+        'test@': {
+          template: "<div><p>hey</p></div>",
+          controller: "PinboardCtrl"
+        }
+      }
+
+    })
+  }
+]);
---
+++ app/assets/javascripts/controllers/pinboard_ctrl.js
@@ -0,0 +1,6 @@
+app.controller("PinboardCtrl", ['$scope', function($scope){
+
+
+  
+
+}]);

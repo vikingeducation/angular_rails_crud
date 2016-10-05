acbe62848fe02b37dad5abdf08251c0987447e22
“Matthew <matthew.hinea@gmail.com>
2016-10-05 15:57:50 -0700

    add all

--- app/assets/javascripts/controllers/PinEditCtrl.js
+++ app/assets/javascripts/controllers/PinEditCtrl.js
@@ -1,9 +1,17 @@
 app.controller("PinEditCtrl", ["$scope", "Pins", "$stateParams", "$state", "Restangular", function($scope, Pins, $stateParams, $state, Restangular) {
 
-  $scope.pin = Pins.find($stateParams.id)
+  new Promise
 
-  $scope.newPin = {}
-  $scope.newPin
+  Pins.find($stateParams.id).then(function(response) {
+    $scope.pin = response.data
+    console.log($scope.pin)
+    $scope.newPin = {}
+    $scope.newPin.item_name = $scope.pin.item_name
+    $scope.newPin.buy_sell = $scope.pin.buy_sell
+    $scope.newPin.description = $scope.pin.description
+    $scope.newPin.user_id = $scope.pin.user_id
+    console.log($scope.newPin)
+  })
 
   $scope.editPin = function() {
     Pins.update($scope.newPin)

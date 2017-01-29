ad71e7fbe9adaf27dde7ed2dc451aa95dae8ae63
Deepak <grc.deepak@gmail.com>
2017-01-29 12:33:43 -0800

    seed User and Pin models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Pin.destroy_all
+
+MULTIPLIER = 10
+puts 'Creating Users'
+(MULTIPLIER).times do
+  User.create(username: Faker::Internet.user_name)
+end
+
+puts 'Creating Pins'
+pins = []
+(MULTIPLIER*5).times do
+  pins << {
+    :item_name => Faker::Commerce.product_name,
+    :buy_sell => rand(2) ? true:false,
+    :description => Faker::Lorem.paragraph,
+    :user_id => User.pluck(:id).sample
+  }
+end
+Pin.create(pins)

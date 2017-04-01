06dd8dd6580e501b2d198d9fec23c960dfd0e620
tt <ttwang.303@gmail.com>
2017-04-01 12:26:13 -0400

    seed and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Clearing database..."
+User.delete_all
+Pin.delete_all
+puts "DONE"
+
+puts "Creating users..."
+20.times do 
+  User.create(
+    username: Faker::App.name
+    )
+end
+puts "DONE"
+
+
+puts "Creating pins..."
+User.all.each do |user|
+  2.times do
+    user.pins.create(
+      item_name: Faker::Hipster.word,
+      description: Faker::Hipster.sentence,
+      buy_sell: false,
+      )
+  end
+end
+
+puts "DONE"
